ebc2f98e09b52d3478461c8bc310a3f9fea57f5f
zora-wuw <54359181+zora-wuw@users.noreply.github.com>
2026-01-09 19:31:09 +1100

    Change logo path in conf.py for Furo theme
    Update logo path for the Furo theme in documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 html_theme = 'furo'
 
 # furo logo
-html_static_path = ["docs/source/figs"]
+html_logo = ["figs"]
 html_theme_options = {
     "light_logo": "logo-light.png",
     "dark_logo": "logo-dark.png",
